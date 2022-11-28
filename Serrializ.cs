62f410722bcea5efdf596d7cf712cf15a99ac252
F_D <den9152@yandex.ru>
2022-11-28 20:53:07 +0700

    xml
    сделан xml

---
+++ Serrializ.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Reflection.Emit;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+
+
+
+
+
+
+
+
+[Serializable]
+    internal class Serrializ
+    {
+
+
+    public int o { get; set; }
+    public string s { get; set; }
+    public string s2 { get; set; }
+    public string s3 { get; set; }
+    public string s4 { get; set; }
+
+    
+    }
+
