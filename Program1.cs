907abe474ae811dda8e2019e0fc97e2c94f224ee
F_D <den9152@yandex.ru>
2022-11-28 20:00:16 +0700

    2
    2

---
+++ Program1.cs
@@ -0,0 +1,34 @@
+﻿
+
+
+
+using System.Text.RegularExpressions;
+using static System.Net.Mime.MediaTypeNames;
+
+internal class Program1
+{
+    private static void Main(string[] args)
+    {
+
+
+        Regex regex = new Regex(@"пок(\w*)");
+        string s = "Расскажите про покупки, про какие покупки? Про покупки, про покупки, Про покупочки мои...";
+        MatchCollection matches = regex.Matches(s);
+        if (matches.Count > 0)
+        {
+            foreach (Match match in matches)
+                Console.WriteLine(match.Value);
+        }
+        else
+        {
+            Console.WriteLine("Совпадений не найдено");
+        }
+        Console.WriteLine(s);
+        Console.WriteLine(matches.Count+" замен с содержанием пок");
+        string target = "Ю";
+        Regex regex1 = new Regex(s);
+        string result = regex.Replace(s, target);
+        Console.WriteLine(result);
+
+    }
+}
